6da73a134c0037956c89325434397554eeea67c7
Riyad DERGUINI <riyadderguini.ML@proton.me>
2024-10-20 19:23:29 +0200

    Fist commit (i.e adding the files in there first state (i.e not finishied))

---
+++ minebayd1/Category.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package minebayd1;
+
+/**
+ * Les catégories permettant de classifier les annonces dans
+ * l'application Minebay.
+ * 
+ * @author Marc Champesme
+ * @since 27/09/2024
+ * @version 27/09/2024
+ */
+public enum Category {
+	CLOTHES, SHOES, BOOKS, GAMES, COMPUTERS
+}
